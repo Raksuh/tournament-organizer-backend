031e81c1a8881b5ba14ebc600e70e80f19dd558a
Jean-Noel Prouteau <jean-noel.prouteau-ext@ima.eu>
2022-08-12 13:53:53 +0200

    Add default role

--- models/user.js
+++ models/user.js
@@ -8,6 +8,10 @@ const userSchema = mongoose.Schema({
   phone: { type: String, required: true },
   password: { type: String, required: true },
   id: { type: String },
+  role: {
+    type: String,
+    default: "player"
+  }
 });
 
 export default mongoose.model('User', userSchema);
